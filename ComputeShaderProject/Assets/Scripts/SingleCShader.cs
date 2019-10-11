17f656094e204aaafa3e50984a2b6663a5062fca
Yuuki Bamba <yuukiBamba@gmail.com>
2019-10-11 10:45:01 +0900

    単一スレッドグループのコンピュートシェーダー及び呼び出しスクリプト。

---
+++ ComputeShaderProject/Assets/Scripts/SingleCShader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sample
+{
+    /// <summary>
+    /// 単一スレッドグループで動作するコンピュートシェーダーの実行モジュール
+    /// </summary>
+    public class SingleCShader : MonoBehaviour
+    {
+        public ComputeShader shader;
+        void Start()
+        {
+            Setup();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void Setup()
+        {
+#if false
+            var th = ThreadSize.GetThreadSize(shader, 0);
+            Debug.Log("x:" + th.x);
+            Debug.Log("y:" + th.y);
+            Debug.Log("z:" + th.z);
+#endif
+            const int c_Loop = 3;
+            ComputeBuffer buffer = new ComputeBuffer(4 * c_Loop, sizeof(int));
+            shader.SetBuffer(0, "buffer", buffer);
+            shader.Dispatch(0, c_Loop, 1, 1);
+            var data = new int[4 * c_Loop];
+            buffer.GetData(data);
+            foreach(var it in data)
+            {
+                Debug.Log("item:" + it);
+            }
+            buffer.Release();
+        }
+    }
+}
