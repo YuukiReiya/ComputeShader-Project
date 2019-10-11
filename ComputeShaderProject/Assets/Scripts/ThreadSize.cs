145f2600696b6050582513a719eb5cb4d162eba1
Yuuki Bamba <yuukiBamba@gmail.com>
2019-10-11 10:43:57 +0900

    スレッドサイズ測定用の構造体追加。

---
+++ ComputeShaderProject/Assets/Scripts/ThreadSize.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct ThreadSize
+{
+    public int x, y, z;
+
+    public ThreadSize(uint x, uint y, uint z)
+    {
+        this.x = (int)x;
+        this.y = (int)y;
+        this.z = (int)z;
+    }
+
+    public static ThreadSize GetThreadSize(ComputeShader cs,int kIndex)
+    {
+        uint x, y, z;
+        cs.GetKernelThreadGroupSizes(kIndex, out x, out y, out z);
+        return new ThreadSize(x, y, z);
+    }
+}
